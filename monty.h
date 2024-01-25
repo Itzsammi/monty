f5699fee7f9e2e155f9e3703a04b531f6c6eb945
Itzsammi <bodmas50@gmail.com>
2024-01-24 17:18:02 -0800

    onit

--- monty.h
+++ monty.h
@@ -1,10 +1,13 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
